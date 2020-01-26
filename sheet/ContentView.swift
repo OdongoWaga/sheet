7e4352dc28b66684693a118fbfd296255cb5ec8b
Waga <wagaodongo@gmail.com>
2020-01-26 13:21:55 +0300

    Show and Dismiss Sheet

--- sheet/ContentView.swift
+++ sheet/ContentView.swift
@@ -6,11 +6,35 @@
 //  Copyright © 2020 JFJ. All rights reserved.
 //
 
+
+
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var name: String
+    
     var body: some View {
-        Text("Hello, World!")
+        Button ("Dismiss") {
+                   //show the sheet
+            self.presentationMode.wrappedValue.dismiss()
+               }
+    }
+}
+
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    
+    var body: some View {
+        Button ("Show Sheet") {
+            //show the sheet
+            self.showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "Waga Odongo")
+        }
     }
 }
 
